e64bf4c69b34419817026729a81145d81172ebf5
Alvin Jaison <Alvin.Jaison@ukhsa.gov.uk>
2023-09-11 10:33:46 +0100

    changed go code

--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/terraform-cdk-go/cdktf"
+    "github.com/aws/constructs-go/constructs/v10"
+    "github.com/aws/jsii-runtime-go"
+    "github.com/hashicorp/terraform-cdk-go/cdktf"
+
+    "github.com/hashicorp/cdktf-provider-docker-go/docker/v3/container"
+    "github.com/hashicorp/cdktf-provider-docker-go/docker/v3/image"
+    dockerprovider "github.com/hashicorp/cdktf-provider-docker-go/docker/v3/provider"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
-	stack := cdktf.NewTerraformStack(scope, &id)
+    stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
+    dockerprovider.NewDockerProvider(stack, jsii.String("docker"), &dockerprovider.DockerProviderConfig{})
 
-	return stack
+    dockerImage := image.NewImage(stack, jsii.String("nginxImage"), &image.ImageConfig{
+        Name:        jsii.String("nginx:latest"),
+        KeepLocally: jsii.Bool(false),
+    })
+
+    container.NewContainer(stack, jsii.String("nginxContainer"), &container.ContainerConfig{
+        Image: dockerImage.Name(),
+        Name:  jsii.String("tutorial"),
+        Ports: &[]*container.ContainerPorts{{
+            Internal: jsii.Number(80), External: jsii.Number(8000),
+        }},
+    })
+
+    return stack
 }
 
 func main() {
-	app := cdktf.NewApp(nil)
+    app := cdktf.NewApp(nil)
 
-	NewMyStack(app, "learn-cdktf-docker")
+    NewMyStack(app, "learn-cdktf-docker")
 
-	app.Synth()
+    app.Synth()
 }
